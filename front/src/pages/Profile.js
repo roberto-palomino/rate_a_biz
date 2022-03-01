157297ba6cbdc5b7b33365deea62acf7246e0ad9
Bihotxi <roberpdlcruz@hotmail.com>
2022-03-01 21:56:18 +0100

    Limpieza de archivo profile

--- front/src/pages/Profile.js
+++ front/src/pages/Profile.js
@@ -1,34 +1,15 @@
 import React, { useContext } from 'react';
-// import EditUserForm from '../components/EditUser/EditUserForm/EditUserForm';
 import EditUser from '../components/EditUser';
-
 import useUserData from '../hooks/useUserData';
 import { TokenContext } from '../index';
-// import { Redirect } from 'react-router';
 
 export const Profile = () => {
   const [token] = useContext(TokenContext);
   const [user] = useUserData(token);
-  console.log('user', user, token);
+
   return (
     <div>
-      <EditUser />
+      <EditUser user={user} />
     </div>
   );
 };
-
-/* export const Profile = () => {
-  const [token] = TokenContext();
-  const [user] = useUserProfile(token);
-
-  if (!token) {
-    return <Redirect to='/login' />;
-  }
-
-  return (
-    <div>
-      <h2> Perfil de usuario</h2>
-      {Object.values(user).length > 0 && <EditUserForm id={user.id} />}
-    </div>
-  );
-}; */
