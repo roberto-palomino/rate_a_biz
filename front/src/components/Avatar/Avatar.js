17a88ff75a7bed0dc11dcd307bac84135dc4474c
Bihotxi <roberpdlcruz@hotmail.com>
2022-04-20 16:25:02 +0200

    Modificado avatar para hacer pruebas

--- front/src/components/Avatar/Avatar.js
+++ front/src/components/Avatar/Avatar.js
@@ -2,30 +2,10 @@ import React from 'react';
 import './Avatar.css';
 import avatarLogo from '../../assets/images/default-avatar.jpeg';
 
-// const Avatar = props => {
-//   const { avatarUrl, username, size, hideFigCaption } = props;
-//   return (
-//     <div className='avatar-container'>
-//       <div className={`avatar ${size === 'medium' ? 'medium' : 'small'}`}>
-//         <img
-//           className='image-avatar'
-//           src={avatarUrl ? avatarUrl : avatarLogo}
-//           alt={`avatar de ${username || 'usuario'}`}
-//         />
-//       </div>
-//       {!hideFigCaption && (
-//         <figcaption className='avatar-username'>{`${
-//           username || 'usuario'
-//         }`}</figcaption>
-//       )}
-//     </div>
-//   );
-// };
-// export default Avatar;
 const Avatar = props => {
-  const { avatarUrl, username, size, figcaption } = props;
+  const { avatarUrl, username, size, hideFigCaption } = props;
   return (
-    <>
+    <div className='avatar-container'>
       <div className={`avatar ${size === 'medium' ? 'medium' : 'small'}`}>
         <img
           className='image-avatar'
@@ -33,12 +13,32 @@ const Avatar = props => {
           alt={`avatar de ${username || 'usuario'}`}
         />
       </div>
-      {figcaption ? (
+      {!hideFigCaption && (
         <figcaption className='avatar-username'>{`${
           username || 'usuario'
         }`}</figcaption>
-      ) : null}
-    </>
+      )}
+    </div>
   );
 };
 export default Avatar;
+// const Avatar = props => {
+//   const { avatarUrl, username, size, figcaption } = props;
+//   return (
+//     <>
+//       <div className={`avatar ${size === 'medium' ? 'medium' : 'small'}`}>
+//         <img
+//           className='image-avatar'
+//           src={avatarUrl ? avatarUrl : avatarLogo}
+//           alt={`avatar de ${username || 'usuario'}`}
+//         />
+//       </div>
+//       {figcaption ? (
+//         <figcaption className='avatar-username'>{`${
+//           username || 'usuario'
+//         }`}</figcaption>
+//       ) : null}
+//     </>
+//   );
+// };
+// export default Avatar;
