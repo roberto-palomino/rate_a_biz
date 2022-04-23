c054352639449684e04a6ae9c0fab3ffeed433c4
Bihotxi <roberpdlcruz@hotmail.com>
2022-04-23 13:28:13 +0200

    Creado archivo de terminos y condiciones. Añadiso a Footer

--- front/src/components/Footer/Footer.js
+++ front/src/components/Footer/Footer.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className='footer-container'>
       <div className='politics-terms'>
-        {/* <p>Política de privacidad</p>
-        <p>Términos y condiciones</p>
-        <p>Política de cookies</p> */}
-        <a href='https://www.google.es'>
-          Política de privacidad. Términos y condiciones{' '}
-        </a>
-
-        <a href='https://www.google.es'>Política de cookies</a>
+        <NavLink to='/TermsAndConditions'>
+          Política de privacidad | Términos y condiciones | Política de cookies
+        </NavLink>
       </div>
       <p className='copyright'>©️2022</p>
     </footer>
