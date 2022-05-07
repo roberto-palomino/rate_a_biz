b9e4c79fc421c9e122bf0a43be97c29fb0c1d142
krizs981 <krizs_lopez@hotmail.com>
2022-05-07 18:10:53 +0200

    añado texto a home

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-
+import { useState } from 'react';
+import _raw from './assets/texts/data.txt';
 import { NavLink } from 'react-router-dom';
 import { useContext } from 'react';
 import { TokenContext } from '.';
 import { TopBusiness } from './components/TopBusiness';
 import Header from './components/Header/Header';
+import { RemoveFromQueue } from '@material-ui/icons';
 
 function App() {
+  const [raw, setRaw] = useState('');
+
+  fetch(_raw)
+    .then((r) => r.text())
+    .then((text) => {
+      setRaw(text);
+    });
   const [token] = useContext(TokenContext);
   let activeStyle = {
     color: 'green',
@@ -21,6 +30,7 @@ function App() {
       <div className='top-reviews'>
         <TopBusiness className='top-preview' />
       </div>
+      <h2>{raw}</h2>
     </div>
   );
 }
